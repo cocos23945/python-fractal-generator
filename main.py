940847e5052bf11f34f80a4b6565e1a58fec1c3b
cocos23945 <feliksvolokitin60@gmail.com>
2025-01-18 16:59:47 +0300

    add slider and Label

---
+++ main.py
@@ -0,0 +1,40 @@
+
+
+class FractalGenerator(tk.Tk):
+    def __(self):
+        super().init()
+
+        self.title("Fractal Generator")
+        self.geometry("800x600")
+
+        self.size = 400
+        self.max_iter= 100
+        self.julia_c = complex(-0.4, 0.6)
+        self.tree_angle = 0.5
+        self.tree_ratio = 0.7
+
+    def setup_ui(self):
+        control_frame = ttk.Frame(self)
+        control_frame.pack(sede=tk.LEFT, fill=tk.Y, padx=5, pady=5)
+
+        ttk.Label(control_frame, text="Тип фракталу: ").pack(pady=5)
+        self.fractal_type = ttk.Combobox(control_frame, value=["Мандельборна", \
+             "Жулія", "Серпінського", "Дерево"])
+        self.fractal_type.set("Мандельборна")
+        self.fractal_type.pack(pady=5)
+        self.fractal_type.bind("<<ComboxSelecte>>", self.on_fractal_change)
+
+        ttk.Label("control_frame, text=:кількість ітеграцій").pack(padu=5) 
+        self.iter_slider = ttk.Scale(control_frame, from_=10, to=200, \
+            orient=tk.HORIZONTAL, value=100, command=self.on_pram_change)
+        self.iter_slider.pack(pady=5)
+        
+        self.julia_frame = ttk.LabelFrame(control_frame, text="Параметри")
+        self.julia_frame.pack(pady=5, fill=tk.X)
+
+        ttk.Label(self.julia_frame, text="Реальна частина c: ").pack()
+        self.julia_real = ttk.Scale(self.julia_frame, from_ =-2, to=2, \
+             orient=tk.HORIZONTAL, value=0.6, coomand=self.on_param_change)
+        self.julia_real.pack()                       
+        
+
